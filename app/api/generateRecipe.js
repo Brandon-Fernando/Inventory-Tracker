8b74fef1cdbec3f05ffdd4f2edec7844d55e4999
Brandon Fernando <brandofernando.3@gmail.com>
2024-08-04 21:58:54 -0400

    finished openai implementation

--- app/api/generateRecipe.js
+++ app/api/generateRecipe.js
@@ -1,7 +1,6 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI({apiKey: "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",
-dangerouslyAllowBrowser: true});
+const openai = new OpenAI({apiKey:  process.env.OPENAI_API_KEY});
 
 export async function generateRecipe(ingredients) {
   const completion = await openai.chat.completions.create({
