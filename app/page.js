a97ec3d75c89376634b37f12f467e040f37d367b
Brandon Fernando <brandofernando.3@gmail.com>
2024-09-22 14:27:59 -0400

    added projects and skills

--- app/page.js
+++ app/page.js
@@ -7,10 +7,21 @@ import {Box, Modal, Typography, Stack, TextField, Button, createTheme, ThemeProv
 import { collection, deleteDoc, getDoc, getDocs, query, setDoc, doc } from "firebase/firestore";
 import Head from 'next/head';
 import styles from "./page.module.css";
+import { Playfair_Display } from "next/font/google";
+
+const playfair = Playfair_Display({
+  weight: '400', 
+  subsets: ['latin'], 
+  display: 'swap',
+});
 
 
 export default function Home() {
   return (
+    <>
+    <head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+    </head>
     <div className={styles.container}>
       <div className={styles.waveContainer}>
         <img src="/wave.svg" alt="Wave" className={styles.wave} />
@@ -24,10 +35,11 @@ export default function Home() {
 
       {/* Hero */}
       <div className={styles.hero}>
-        <h1>WHAT'S IN YOUR PANTRY?</h1>
+        <h1 style={{fontFamily: playfair.style.fontFamily}}>WHAT'S IN YOUR PANTRY?</h1>
         <p>Keep track of what’s in your pantry and generate recipes based on what’s in it.</p>
         <button>Get Started</button>
       </div>
     </div>
+    </>
   );
 }
